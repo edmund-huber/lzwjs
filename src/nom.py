fbb73bee13fb9dae53ddb95f159b83c1f0e511bd
Edmund Huber <edmund.pgh@gmail.com>
2010-08-11 23:28:49 -0700

    Variable width LZW

--- src/nom.py
+++ src/nom.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import base64
+import math
 import struct
 import sys
 
@@ -21,6 +22,7 @@ class BitWriter:
         self.bytes = []
         self.bits = 0
         self.count = 0
+	self.written = 0
     def write_bit(self, bit):
         self.bits = self.bits | (bit << (7 - self.count))
         self.count += 1
@@ -28,6 +30,7 @@ class BitWriter:
             self.bytes.append(self.bits)
             self.bits = 0
             self.count = 0
+	self.written += 1
     def write(self, add_bits, add_count):
         for i in range(0, add_count):
             self.write_bit(1 & (add_bits >> (add_count - i - 1)))
@@ -41,29 +44,22 @@ writer = BitWriter()
 w = input[0]
 d = dict([(chr(c), c) for c in range(0, 256)])
 c = 256
-encoded = []
+o = 0
 if len(input[1:]) > 0:
     for i in input[1:]:
         wi = w + i
         if wi in d:
             w = wi
         else:
-            encoded.append(d[w])
-            writer.write(d[w], 16)
+            writer.write(d[w], int(math.ceil(math.log(c, 2))))
+            o += 1
             d[wi] = c
             c += 1
             w = i
-    encoded.append(d[w])
-    writer.write(d[w], 16)
-
-#print ''.join([struct.pack('H', c) for c in encoded])
-print str(writer)
-
-assert False
-
-encoded = str(writer)
-
-# 
+    writer.write(d[w], int(math.ceil(math.log(c, 2))))
+    o += 1
+print >> sys.stderr, 'outputted %s codes' % str(o)
+encoded = struct.pack('H', o) + str(writer)
 
 # output base64/LZW decoder
 
@@ -100,16 +96,38 @@ var from_base64 = function(s) {
 };
 
 var decode_lzw = function(d) {
+	// get the header -- number of codes
+	var total = (d[1] << 8) | d[0];
+
+	// initialize the lookup table
 	var table = {};
 	for (var i = 0; i < 256; i++) {
 		table[i] = String.fromCharCode(i);
 	}
-	var old_c = (d[1] << 8) | d[0];
+
+	var getBits = function(s, p, c) {
+		var getBit = function(s, p) {
+			var b = s[Math.floor(p / 8)];
+			return (b & (1 << (7 - (p %% 8)))) > 0 ? 1 : 0;
+		};
+		var bits = 0;
+		for (var i = 0; i < c; i++) {
+			bits = bits | (getBit(s, p + i) << (c - i - 1));
+		}
+		return bits;
+	};
+
+	// decode
+	var old_c = getBits(d, 16, 8);
 	var decoded = table[old_c];
+	var pos = 24;
+	var wid = 8;
 	var c = old_c;
 	var count = 256;
-	for (var i = 2; i < d.length; i+=2) {
-		var new_c = (d[i+1] << 8) | d[i+0];
+	for (var i = 0; i < total; i++) {
+		wid = Math.ceil(Math.log(count + 1) / Math.log(2));
+		var new_c = getBits(d, pos, wid);
+		pos += wid;
 		var s;
 		if (!table[new_c]) {
 			s = table[old_c] + c;
